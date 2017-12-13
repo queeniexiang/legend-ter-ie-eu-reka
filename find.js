aa8faca2d6d3c0c4094d5b431c698f1a99575637
queeniexiang <wxiang1@stuy.edu>
2017-12-13 13:13:27 -0500

    Finished

--- find.js
+++ find.js
@@ -1,38 +1,56 @@
+/* 
+Team legend-ter-ie-eu-reka
+Queenie Xiang, Eugene Thomas, Terry Guan 
+Softdev pd7
+HW17: Moo? 
+2017-12-13
+*/
+
 var box = document.getElementById("box");
 var boxHeight = box.offsetHeight;
 var boxWidth = box.offsetWidth;
 
-//hardcode target as center
-//randomize later
+
+//Randomized target location 
+var targetX = Math.floor(Math.random() * boxWidth) + 1;
+var targetY = Math.floor(Math.random()* boxHeight) + 1; 
+
+/* 
+//Hard code target as center 
 var targetX = boxWidth / 2;
 var targetY = boxHeight / 2;
-
+*/
 
 console.log( "box height: " + boxHeight );
 console.log( "box width: " + boxWidth );
+console.log( "Target location X: " + targetX);
+console.log( "Target location Y: " + targetY);
 
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
-  var xcor = Math.pow( (x0 - x1), 2);
-  var ycor = Math.pow( (y0 - y1), 2);
-  return Math.sqrt(xcor + ycor);
+    var xcor = Math.pow( (x0 - x1), 2);
+    var ycor = Math.pow( (y0 - y1), 2);
+    return Math.sqrt(xcor + ycor);
 };
-
 
 var findIt = function(e) {
-  var currentX = event.clientX;
-  var currentY = event.clientY;
-  console.log("mouse x: " + currentX + " mouse y: " + currentY);
-  var dist = distance(currentX, currentY, targetX, targetY);
-  console.log("distance: " + dist);
-  dist = Math.floor(255 - dist);
-  document.body.style.background = "rgb(" + dist + "," + dist + "," + dist + ")";
+    var currentX = e.clientX;
+    var currentY = e.clientY;
+    //console.log("mouse x: " + currentX + " mouse y: " + currentY);
 
+    //Distance 
+    var dist = distance(currentX, currentY, targetX, targetY);
+    console.log("distance: " + dist);
+
+    //Colors
+    dist_rgb = Math.floor(255-dist);
+    //Setting the rgb colors using current distance; 
+    document.body.style.background = "rgb(0, 0" +  "," + dist_rgb + ")";
+    
+    if (dist < 5) {
+	document.body.style.background = "gold";
+	console.log("Target found!");
+    }
 };
-
-/*
-your OTHER FXNS
-
-*/
 
 box.addEventListener("mousemove", findIt);
